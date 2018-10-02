3cc34933b1550775dffc5de7984b69699a6fd5d3
= <nncl@live.com>
2018-10-01 23:03:13 -0300

    Adding switch to the route

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import {BrowserRouter as Router, Route, Link, Switch} from "react-router-dom";
 
 ReactDOM.render(
     <Router>
-        <Route exact path='/' component={App} />
+        <Switch>
+            <Route exact path='/' component={App}/>
+        </Switch>
     </Router>,
     document.getElementById('root')
 );
